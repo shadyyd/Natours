f04c24c78148c5e30acb3216419d2fbfacf0de72
Shady Radwan <yd.radwan@gmail.com>
2024-05-04 17:59:18 +0300

    Improved stripe implementation

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -8,13 +8,14 @@ const router = express.Router();
 
 router.get(
   '/',
-  bookingController.createBookingCheckout,
+  // bookingController.createBookingCheckout,
   authController.isLoggedIn,
   viewsController.getOverview,
 );
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
 router.get('/me', authController.protect, viewsController.getAccount);
+
 router.get('/my-tours', authController.protect, viewsController.getMyTours);
 
 router.post(
